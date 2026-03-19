b5f134674052ccd1fd6862bfa7d71d994414d4cb
savejobar <normchel1990@gmail.com>
2026-03-19 15:06:46 +0300

    Add requirements_2

--- Forecast/app/sidebar.py
+++ Forecast/app/sidebar.py
@@ -59,10 +59,10 @@ def _run_pipeline(f1, f2) -> None:
                 p1 = os.path.join(tmp, "repair.xlsx")
                 p2 = os.path.join(tmp, "stock.xlsx")
 
-                with open(p1, "wb") as fh:
-                    fh.write(f1.read())
-                with open(p2, "wb") as fh:
-                    fh.write(f2.read())
+            with open(p1, "wb") as fh:
+                fh.write(f1.getvalue())  # getvalue() не зависит от позиции курсора
+            with open(p2, "wb") as fh:
+                fh.write(f2.getvalue())
 
                 df = run_full_pipeline(repair_path=p1, stock_path=p2)
 
